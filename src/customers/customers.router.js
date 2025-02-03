a6171cc531462af282d1d7aaaf920d7a8e0bbc22
chen1720 <chen1720@purdue.edu>
2025-02-02 20:21:54 -0800

    fix cusomters route

--- src/customers/customers.router.js
+++ src/customers/customers.router.js
@@ -3,14 +3,10 @@ const controller = require("./customers.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // Add the appropriate methods in this file
-// Hint: You can use router.route() to chain route methods that share the same path  
+// Hint: You can use router.route() to chain route methods that share the same path
 
-router
-  .route("/")
-  .post(controller.create)
+router.route("/").post(controller.create);
 
-router
-  .route("/:customerId")
-  .get(controller.read)
+router.route("/:customerId").get(controller.rsead);
 
 module.exports = router;
