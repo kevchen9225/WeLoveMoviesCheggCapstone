5157c33bf13b95e5f20dd5aa181af0afb321ec07
chen1720 <chen1720@purdue.edu>
2025-02-10 22:04:22 -0800

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
@@ -9,6 +10,7 @@ const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
 // TODO: Add your code here
+app.use(cors());
 app.use(express.json());
 
 app.use("/theaters", theatersRouter);
